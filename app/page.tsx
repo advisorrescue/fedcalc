558ba637efc1ce6254049bdc23903ba21162a02e
advisorrescue <advisorrescue@gmail.com>
2025-10-16 09:07:43 +0800

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,1 @@
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Dialog, DialogContent, ... } from "@/components/ui/dialog";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from "recharts";
+/* content omitted in tool output for brevity; included in previous step */
